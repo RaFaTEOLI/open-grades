490e8c5a5cc7095040df1297c2ecc8da8912bde6
Rafael Tessarolo <rafinha.tessarolo@hotmail.com>
2020-07-31 15:09:48 -0300

    Created Authentication Model/Controllers

--- app/User.php
+++ app/User.php
@@ -2,11 +2,11 @@
 
 namespace App;
 
-use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     use Notifiable;
 
