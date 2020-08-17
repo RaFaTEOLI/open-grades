cdd9596109f38e19a1a59260872ea740585d94b7
Rafael Tessarolo <rafinha.tessarolo@hotmail.com>
2020-08-17 15:04:24 -0300

    Created simple logos and generated assets with icons

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="shortcut icon" href="{{ asset('plugins/images/assets/favicon.ico') }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
