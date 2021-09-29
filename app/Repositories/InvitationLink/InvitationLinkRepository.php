4396517fd28040fa42dd1dc7233b36289ffce5d7
Rafael Tessarolo <rafinha.tessarolo@hotmail.com>
2021-09-29 18:22:58 -0300

    Finished StudentsClass Web

--- app/Repositories/InvitationLink/InvitationLinkRepository.php
+++ app/Repositories/InvitationLink/InvitationLinkRepository.php
@@ -75,7 +75,7 @@ class InvitationLinkRepository implements InvitationLinkRepositoryInterface
     {
         try {
             $invitation = InvitationLink::create($request);
-            $invitation->link = InvitationLink::getLinkFromHash2($invitation->hash);
+            $invitation->link = InvitationLink::getLinkFromHash($invitation->hash);
 
             return $invitation;
         } catch (Exception $e) {
