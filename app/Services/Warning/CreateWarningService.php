a675273a5dd4b02a6100c8d4464fd92cc30fd125
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2022-08-05 18:27:29 -0300

    refactor: add ItemNotFoundException to CreateWarningService

--- app/Services/Warning/CreateWarningService.php
+++ app/Services/Warning/CreateWarningService.php
@@ -2,16 +2,14 @@
 
 namespace App\Services\Warning;
 
-use App\Exceptions\UserNotStudent;
-use App\Models\StudentsResponsible;
 use App\Models\User;
 use App\Notifications\StudentWarning;
 use App\Repositories\Student\StudentRepository;
 use App\Repositories\StudentsResponsible\StudentsResponsibleRepository;
 use App\Repositories\Warning\WarningRepository;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\ItemNotFoundException;
 use Exception;
-use Illuminate\Support\Facades\Notification;
 
 class CreateWarningService
 {
@@ -26,21 +24,21 @@ class CreateWarningService
     {
         try {
             $student = (new StudentRepository())->findById($request['student_id']);
-            if (!empty($student)) {
-                $warningRepository = new WarningRepository();
-                $request['reporter_id'] = Auth::user()->id;
 
-                $warning = $warningRepository->store($request)->format();
-                $responsibles = (new StudentsResponsibleRepository())->findByStudentId($request['student_id']);
-                if (!empty($responsibles)) {
-                    foreach ($responsibles as $responsible) {
-                        $this->objectToCollection($responsible)->notify(new StudentWarning($student, $warning));
-                    }
+            $warningRepository = new WarningRepository();
+            $request['reporter_id'] = Auth::user()->id;
+
+            $warning = $warningRepository->store($request)->format();
+            $responsibles = (new StudentsResponsibleRepository())->findByStudentId($request['student_id']);
+            if (!empty($responsibles)) {
+                foreach ($responsibles as $responsible) {
+                    $this->objectToCollection($responsible)->notify(new StudentWarning($student, $warning));
                 }
-
-                return $warning;
             }
-            throw new UserNotStudent();
+
+            return $warning;
+        } catch (ItemNotFoundException $i) {
+            throw new ItemNotFoundException($i->getMessage(), $i->getCode());
         } catch (Exception $e) {
             throw new Exception($e->getMessage());
         }
