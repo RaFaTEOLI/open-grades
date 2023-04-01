46a6aaaeb63246d36048afb5d9c7a980101cb389
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2023-04-01 15:58:37 -0300

    refactor: remove unnecessary functions

--- app/Repositories/User/UserRepositoryInterface.php
+++ app/Repositories/User/UserRepositoryInterface.php
@@ -8,12 +8,10 @@ use App\Models\User;
 interface UserRepositoryInterface
 {
     public function all(int $limit = 0, int $offset = 0): Collection | array;
-    public function allStudents(): Collection;
     public function store(array $request): User;
     public function findById(int $id): object;
     public function update(int $userId, array $set): void;
     public function delete(int $userId): bool;
     public function createType(string $type, int $userId): void;
-    public function allResponsibles(): Collection;
     public function allButAdmin(): Collection;
 }
